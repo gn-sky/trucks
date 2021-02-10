f62553f212773a6a165a11dd4f665a6d6c9ae9f7
eugene.sky <skytech4dotnet@gmail.com>
2021-02-10 17:53:07 -0600

    added truck.module and started component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TruckListComponent } from './truck/truck-list/truck-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TruckListComponent },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/truck/truck.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TruckRoutingModule } from './truck-routing.module';
+import { TruckListComponent } from './truck-list/truck-list.component';
+
+
+@NgModule({
+  declarations: [TruckListComponent],
+  imports: [
+    CommonModule,
+    TruckRoutingModule
+  ],
+  exports: [TruckListComponent]
+})
+export class TruckModule { }
